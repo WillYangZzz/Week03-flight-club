b23ee2b7c77408df6ad754c79606f103bb68dcc3
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-07 20:21:42 +1200

    fetch qr image dynamically

--- server.js
+++ server.js
@@ -15,17 +15,21 @@ server.set('view engine', 'hbs')
 server.set('views', Path.join(__dirname, 'views'))
 server.use(express.static(Path.join(__dirname, 'public')))
 
-server.get('/', async (req, res) => {
-  // const ticketNo = Math.floor(Math.random() * 1000000000)
-  // const response = await fetch(
-  //   `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${ticketNo}`
-  // )
-  //
-  // // convert image to base64
-  // const buffer = await response.arrayBuffer()
-  // const base64Data = `data:image/png;base64,${Buffer.from(buffer).toString(
-  //   'base64'
-  // )}`
+async function getQrImage(ticketNo) {
+  const response = await fetch(
+    `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${ticketNo}`
+  )
+
+  // convert image to base64
+  const buffer = await response.arrayBuffer()
+  return `data:image/png;base64,${Buffer.from(buffer).toString('base64')}`
+}
+
+server.get('/:ticket', async (req, res) => {
+  // TODO: call db function to get ticket data
+
+  const ticketNo = req.params.ticket || '1234567890'
+  const base64Data = await getQrImage(ticketNo)
 
   const viewData = {
     name: 'Jenny Rosen',
@@ -39,7 +43,7 @@ server.get('/', async (req, res) => {
     gate: '22',
     departure: '09 Aug 2020 10:00',
     arrival: '10 Aug 2020 17:00',
-    img: '',
+    img: base64Data,
   }
 
   res.render('ticket', viewData)
