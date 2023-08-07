9addd4140a0c084923d7b7da2417680a76799302
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-08 06:59:56 +1200

    call api directly in hbs view

--- server/server.js
+++ server/server.js
@@ -15,21 +15,10 @@ server.set('view engine', 'hbs')
 server.set('views', Path.join(__dirname, 'views'))
 server.use(express.static(Path.join(__dirname, '..', 'public')))
 
-async function getQrImage(ticketNo) {
-  const response = await fetch(
-    `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${ticketNo}`
-  )
-
-  // convert image to base64
-  const buffer = await response.arrayBuffer()
-  return `data:image/png;base64,${Buffer.from(buffer).toString('base64')}`
-}
-
 server.get('/:ticket', async (req, res) => {
   // TODO: call db function to get ticket data
 
   const ticketNo = req.params.ticket || '1234567890'
-  const base64Data = await getQrImage(ticketNo)
 
   const viewData = {
     name: 'Jenny Rosen',
@@ -39,11 +28,10 @@ server.get('/:ticket', async (req, res) => {
     date: '05 Aug 2020',
     seat: '14B',
     class: 'Business',
-    ticketNo: '10',
+    ticketNo,
     gate: '22',
     departure: '09 Aug 2020 10:00',
     arrival: '10 Aug 2020 17:00',
-    img: base64Data,
   }
 
   res.render('ticket', viewData)
