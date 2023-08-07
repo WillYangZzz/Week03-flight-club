752dfdf684a0cef4da5a7823c97f0958f6c224ae
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-07 14:55:19 +1200

    remove express server

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const server = require('./server')
-
-const port = process.env.PORT || 3000
-
-server.listen(port, () => {
-  console.log('Server is listening on port', port)
-})
+console.log('Run npm run test')
