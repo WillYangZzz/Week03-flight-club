1d03508974c1bcb477064337d2ee9c282f9bf1fc
Kirsty <142766386+Kirsty-Ammundsen@users.noreply.github.com>
2023-10-24 15:25:57 +1300

    complete count function

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,22 @@
 import db from './connection.js'
 
 export async function getMyTickets(passengerId) {
-  return db('tickets').where('passenger_id', passengerId).select()
+  return db('tickets').where('passenger_id', passengerId)
 }
 
 export async function getMyTicketsByDob(dob) {
   return await db('tickets')
     .join('passengers', 'tickets.passenger_id', 'passengers.id')
     .where('passengers.dob', dob)
-    .select()
 }
 
-export async function countMyTicketsByDob(dob) {}
+export async function countMyTicketsByDob(dob) {
+  const tickets = await db('tickets')
+    .join('passengers', 'tickets.passenger_id', 'passengers.id')
+    .where('passengers.dob', dob)
+
+  return { count: tickets.length }
+}
 
 export async function countMyLostLuggage(dob) {}
 
