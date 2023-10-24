e5ec7b3d4d561e07682667b454ee8bc25d83afae
Samir Lekinwala <samirlekinwala@gmail.com>
2023-10-24 19:23:31 +1300

    Complete queries for test

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,50 @@
 import db from './connection.js'
 
-export async function getMyTickets(passengerId) {}
+export async function getMyTickets(passengerId) {
+  return db('tickets').where('passenger_id', passengerId)
+}
 
-export async function getMyTicketsByDob(dob) {}
+export async function getMyTicketsByDob(dob) {
+  return db('passengers')
+    .join('tickets', 'tickets.passenger_id', 'passengers.id')
+    .where('passengers.dob', dob)
+}
 
-export async function countMyTicketsByDob(dob) {}
+export async function countMyTicketsByDob(dob) {
+  return db('passengers')
+    .join('tickets', 'tickets.passenger_id', 'passengers.id')
+    .where('passengers.dob', dob)
+    .count('tickets.id as count')
+    .first()
+}
 
-export async function countMyLostLuggage(dob) {}
+export async function countMyLostLuggage(dob) {
+  return db('luggage')
+    .join('tickets', 'ticket_id', 'tickets.id')
+    .join('passengers', 'passenger_id', 'passengers.id')
+    .where('passengers.dob', dob)
+    .where('is_lost', true)
+    .count('luggage.id as count')
+    .first()
+}
 
-export async function sumMyLostLuggageWeight(dob) {}
+export async function sumMyLostLuggageWeight(dob) {
+  return db('luggage')
+    .join('tickets', 'ticket_id', 'tickets.id')
+    .join('passengers', 'passenger_id', 'passengers.id')
+    .where('passengers.dob', dob)
+    .where('luggage.is_lost', true)
+    .sum('weight as sum')
+    .first()
+}
 
-export async function getMyLostLuggageLocation(dob) {}
+export async function getMyLostLuggageLocation(dob) {
+  return db('luggage')
+    .join('tickets', 'ticket_id', 'tickets.id')
+    .join('passengers', 'passenger_id', 'passengers.id')
+    .join('airports', 'located_airport_id', 'airports.id')
+    .where('passengers.dob', dob)
+    .where('luggage.is_lost', true)
+    .select('airports.phone', 'airports.email')
+    .first()
+}
