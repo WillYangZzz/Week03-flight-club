7e835395b69961af73fe89bebc643545432809b5
Dan <143304904+danial-shahali@users.noreply.github.com>
2023-10-24 16:15:11 +1300

    completed 1-6 challenges

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,50 @@
 import db from './connection.js'
 
-export async function getMyTickets(passengerId) {}
+export async function getMyTickets(passengerId) {
+  return db('tickets').where('passenger_id', passengerId)
+}
 
-export async function getMyTicketsByDob(dob) {}
+export async function getMyTicketsByDob(dob) {
+  return db('tickets')
+    .join('passengers', 'tickets.passenger_id', 'passengers.id')
+    .where('passengers.dob', dob)
+}
 
-export async function countMyTicketsByDob(dob) {}
+export async function countMyTicketsByDob(dob) {
+  return db('tickets')
+    .join('passengers', 'tickets.passenger_id', 'passengers.id')
+    .where('passengers.dob', dob)
+    .count({ count: 'tickets.id' })
+    .first()
+}
 
-export async function countMyLostLuggage(dob) {}
+export async function countMyLostLuggage(dob) {
+  return db('tickets')
+    .join('passengers', 'tickets.passenger_id', 'passengers.id')
+    .join('luggage', 'tickets.id', 'luggage.ticket_id')
+    .where('passengers.dob', dob)
+    .where('is_lost', true)
+    .count({ count: 'tickets.id' })
+    .first()
+}
 
-export async function sumMyLostLuggageWeight(dob) {}
+export async function sumMyLostLuggageWeight(dob) {
+  return db('tickets')
+    .join('passengers', 'tickets.passenger_id', 'passengers.id')
+    .join('luggage', 'tickets.id', 'luggage.ticket_id')
+    .where('passengers.dob', dob)
+    .where('is_lost', true)
+    .sum({ sum: 'luggage.weight' })
+    .first()
+}
 
-export async function getMyLostLuggageLocation(dob) {}
+export async function getMyLostLuggageLocation(dob) {
+  return db('tickets')
+    .join('passengers', 'tickets.passenger_id', 'passengers.id')
+    .join('luggage', 'tickets.id', 'luggage.ticket_id')
+    .join('airports', 'airports.id', 'luggage.located_airport_id')
+    .select('airports.phone', 'airports.email')
+    .where('is_lost', true)
+    .where('passengers.dob', dob)
+    .first()
+}
