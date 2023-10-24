8f9ec843b3b62d74e43148887e1fc7eaedb6941e
Vitor <142807308+vitor-fernandess@users.noreply.github.com>
2023-10-24 16:08:02 +1300

    count my last luggage

--- server/db/index.js
+++ server/db/index.js
@@ -4,11 +4,29 @@ export async function getMyTickets(passengerId) {
   return db('tickets').where('passenger_id', passengerId)
 }
 
-export async function getMyTicketsByDob(dob) {}
+export async function getMyTicketsByDob(dob) {
+  return db('tickets')
+    .join('passengers', 'tickets.passenger_id', 'passengers.id')
+    .where('passengers.dob', dob)
+}
 
-export async function countMyTicketsByDob(dob) {}
+export async function countMyTicketsByDob(dob) {
+  return db('tickets')
+    .join('passengers', 'tickets.passenger_id', 'passengers.id')
+    .where('passengers.dob', dob)
+    .count({ count: 'tickets.id' })
+    .first()
+}
 
-export async function countMyLostLuggage(dob) {}
+export async function countMyLostLuggage(dob) {
+  return db('tickets')
+    .join('passengers', 'tickets.passenger_id', 'passengers.id')
+    .join('luggage', 'tickets.id', 'luggage.ticket_id')
+    .where('passengers.dob', dob)
+    .where('is_lost', true)
+    .count({ count: 'tickets.id' })
+    .first()
+}
 
 export async function sumMyLostLuggageWeight(dob) {}
 
