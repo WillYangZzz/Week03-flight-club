a70eafe5ee3fd80b3cd9c72f1b52ca0bb62edbb0
Xavier <143090232+xavier-kirkpatrick@users.noreply.github.com>
2023-10-24 17:51:20 +1300

    finished day

--- server/db/index.js
+++ server/db/index.js
@@ -38,4 +38,12 @@ export async function sumMyLostLuggageWeight(dob) {
     .first()
 }
 
-export async function getMyLostLuggageLocation(dob) {}
+export async function getMyLostLuggageLocation(dob) {
+  return db('passengers')
+    .join('tickets', 'tickets.passenger_id', 'passengers.id')
+    .join('luggage', 'luggage.ticket_id', 'tickets.id')
+    .join('airports', 'airports.id', 'luggage.located_airport_id')
+    .where('passengers.dob', dob)
+    .where('luggage.is_lost', true)
+    .select()
+}
